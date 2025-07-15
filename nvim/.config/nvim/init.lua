999f59054672f7081a35107ee0afec2de6d09ad8
Piotr <piotreksmolinski04@gmail.com>
2025-07-15 17:28:25 +0200

    fix: format tsx

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,6 +23,8 @@ vim.o.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = "a"
 
+vim.o.relativenumber = true
+
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -271,6 +271,7 @@ return {
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				typescriptreact = { "prettier" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
